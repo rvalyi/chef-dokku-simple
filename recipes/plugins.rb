b4c4e72bdbb7c4b404e9eba1b1e985e906ae3ef8
Raphael Valyi <rvalyi@akretion.com>
2015-10-07 02:18:58 -0300

    fix plugin installation on 0.4

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -1,27 +1,24 @@
-## download requested plugins
 node[:dokku][:plugins].each do |name, url|
-  target_dir = "/var/lib/dokku/plugins/#{name}"
+  target_dir = "/tmp/dokku-plugin-#{name}"
 
   if url.to_s == "remove"
-    directory target_dir do
-      recursive true
-      action :delete
+    bash 'dokku-plugin-uninstall' do
+      code "dokku plugin:uninstall #{name}"
     end
   else
+    ## download requested plugin
     (url, rev) = url.split("#", 2) if url.include?("#")
-
     git target_dir do
       repository url
       revision rev if rev
       action :sync
-      notifies :run, 'bash[dokku-plugins-install]'
+      notifies :run, "bash[dokku-plugin-install-#{name}]"
     end
   end
-end
 
-## install all plugins
-bash 'dokku-plugins-install' do
-  cwd '/var/lib/dokku/plugins'
-  code 'dokku plugins-install'
-  action :nothing
+  ## install plugin
+  bash "dokku-plugin-install-#{name}" do
+    code "dokku plugin:update #{target_dir}"
+    action :nothing
+  end
 end
