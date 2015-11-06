e5e8380df70f2d70ae07d73411535a1f24e893e1
Jean Jacques Warmerdam <sjaq@koelka.st>
2015-11-06 18:05:59 +0100

    Fix conditional logic when update and when install

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -2,21 +2,23 @@ node[:dokku][:plugins].each do |name, url|
   if url.to_s == "remove"
     if plugin_list.include?(name)
       bash 'dokku-plugin-uninstall' do
+        user 'root'
         code "dokku plugin:uninstall #{name}"
       end
     end
   else
     ## install plugin
     bash "dokku-plugin-install-#{name}" do
+      user 'root'
       code "dokku plugin:install #{url}"
-      only_if "dokku plugin | grep #{name}", user: "root"
+      not_if "dokku plugin | grep #{name}", user: "root"
     end
 
     # Disable plugin update as updating plugins is currently broken in plugn itself
     # See: https://github.com/progrium/dokku/issues/1531
     # bash "dokku-plugin-update-#{name}" do
     #   code "dokku plugin:update #{url}"
-    #   not_if "dokku plugin | grep #{name}", user: "root"
+    #   only_if "dokku plugin | grep #{name}", user: "root"
     # end
   end
 end
