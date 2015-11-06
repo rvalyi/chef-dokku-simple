cd9287b55767f0010a7ea033a8faf1612cf34918
Jean Jacques Warmerdam <sjaq@koelka.st>
2015-11-06 11:10:45 +0100

    Hopefully finally fix plugin installation

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -1,25 +1,20 @@
-ruby_block "dokku-plugin-install" do
-  block do
-    list = Chef::ShellOut.new('dokku', 'plugin', user: 'root')
-    list.run_command
-    list.error!
-    installed_plugins = list.stdout.split("\n").map do |line|
-      line.split('  ').map(&:strip).reject(&:empty?).first
+node[:dokku][:plugins].each do |name, url|
+  if url.to_s == "remove"
+    if plugin_list.include?(name)
+      bash 'dokku-plugin-uninstall' do
+        code "dokku plugin:uninstall #{name}"
+      end
+    end
+  else
+    ## install plugin
+    bash "dokku-plugin-install-#{name}" do
+      code "dokku plugin:install #{url}"
+      only_if "dokku plugin | grep #{name}", user: "root"
     end
 
-    node[:dokku][:plugins].each do |name, url|
-      if url.to_s == "remove"
-        if plugin_list.include?(name)
-          bash 'dokku-plugin-uninstall' do
-            code "dokku plugin:uninstall #{name}"
-          end
-        end
-      else
-        ## install plugin
-        bash "dokku-plugin-install-#{name}" do
-          code "dokku plugin:#{list.include?(name) ? 'update' : 'install'} #{url}"
-        end
-      end
+    bash "dokku-plugin-update-#{name}" do
+      code "dokku plugin:update #{url}"
+      not_if "dokku plugin | grep #{name}", user: "root"
     end
   end
 end
