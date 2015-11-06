312a8d55874c962b9bca026d2d913bfa3940637e
Jean Jacques Warmerdam <sjaq@koelka.st>
2015-11-06 01:37:52 +0100

    Probably not the nicest way of doing this but better hope it works

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -1,19 +1,25 @@
-plugin_list_call = Chef::ShellOut.new('dokku', 'plugin', user: 'root')
-plugin_list_call.run_command
-plugin_list_call.error!
-plugin_list = plugin_list_call.stdout.split("\n").map { |line| line.split('  ').map(&:strip).reject(&:empty?).first }
-
-node[:dokku][:plugins].each do |name, url|
-  if url.to_s == "remove"
-    if plugin_list.include?(name)
-      bash 'dokku-plugin-uninstall' do
-        code "dokku plugin:uninstall #{name}"
-      end
+ruby_block "dokku-plugin-install" do
+  block do
+    list = Chef::ShellOut.new('dokku', 'plugin', user: 'root')
+    list.run_command
+    list.error!
+    installed_plugins = list.stdout.split("\n").map do |line|
+      line.split('  ').map(&:strip).reject(&:empty?).first
     end
-  else
-    ## install plugin
-    bash "dokku-plugin-install-#{name}" do
-      code "dokku plugin:#{plugin_list.include?(name) ? 'update' : 'install'} #{url}"
+
+    node[:dokku][:plugins].each do |name, url|
+      if url.to_s == "remove"
+        if plugin_list.include?(name)
+          bash 'dokku-plugin-uninstall' do
+            code "dokku plugin:uninstall #{name}"
+          end
+        end
+      else
+        ## install plugin
+        bash "dokku-plugin-install-#{name}" do
+          code "dokku plugin:#{list.include?(name) ? 'update' : 'install'} #{url}"
+        end
+      end
     end
   end
 end
